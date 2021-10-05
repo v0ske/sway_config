3eeb75361d5f7250816d4ce39115087a9e826c36
laurent08 <laurent08@inuits.eu>
2021-10-05 10:23:34 +0200

    add proper input device

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-ln -s /home/laurent08/sway/sway /home/laurent08/.config/sway
-ln -s /home/laurent08/swayswaync /home/laurent08/.config/swaync
-ln -s /home/laurent08/sway/waybar /home/laurent08/.config/waybar
-ln -s /home/laurent08/sway/config.d /etc/sway/config.d
+ln -sfr /home/laurent08/sway/sway /home/laurent08/.config/sway
+ln -sfr /home/laurent08/sway/swaync /home/laurent08/.config/swaync
+ln -sf /home/laurent08/sway/waybar /home/laurent08/.config/waybar
+ln -sf /home/laurent08/sway/config.d /etc/sway/config.d
