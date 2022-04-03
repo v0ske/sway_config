19f17ae49945294e2d3ce0ca1cdd0acb75f36efb
laurent08 <laurent08@inuits.eu>
2022-04-03 21:57:31 +0200

    update install script + keybinds

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ fi
 ln -sfr /home/laurent08/sway/sway /home/laurent08/.config/sway
 ln -sfr /home/laurent08/sway/swaync /home/laurent08/.config/swaync
 ln -sf /home/laurent08/sway/waybar /home/laurent08/.config/waybar
-ln -sf /home/laurent08/sway/config.d /etc/sway/config.d
+ln -sf /home/laurent08/sway/config.d/* /etc/sway/config.d
